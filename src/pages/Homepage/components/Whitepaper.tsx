788574156778adae62b7d88fc91e9b7c7bf4ddc2
lukachi <lukachisama@gmail.com>
2024-05-17 19:03:00 +0300

    whitepaper section basics

--- src/pages/Homepage/components/Whitepaper.tsx
+++ src/pages/Homepage/components/Whitepaper.tsx
@@ -1,13 +1,35 @@
 import { HTMLAttributes } from 'react'
+import { useTranslation } from 'react-i18next'
 
-import { UiContainer } from '@/ui'
+import { RoutePaths } from '@/enums'
+import { cn } from '@/theme/utils'
+import { UiContainer, UiIconLink } from '@/ui'
 
 type Props = HTMLAttributes<HTMLDivElement>
 
 export default function Whitepaper({ ...rest }: Props) {
+  const { t } = useTranslation()
+
   return (
     <div {...rest}>
-      <UiContainer>WHITEPAPER</UiContainer>
+      <UiContainer>
+        <div className={cn('flex flex-col text-center')}>
+          <h4 className={cn('text-h4')}>{t('whitepaper.title')}</h4>
+
+          <div className={cn('flex flex-col text-center')}>
+            <span className='text-subtitle1'>{t('whitepaper.text')}</span>
+
+            <span className='text-subtitle1'>{`* * *`}</span>
+          </div>
+
+          <UiIconLink
+            className={cn('self-center mt-4')}
+            route={RoutePaths.techDoc}
+          >
+            {t('whitepaper.link')}
+          </UiIconLink>
+        </div>
+      </UiContainer>
     </div>
   )
 }
---
+++ src/ui/UiIconLink.tsx
@@ -0,0 +1,53 @@
+import { HTMLAttributes } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { IconNames } from '@/enums'
+import { cn } from '@/theme/utils'
+import { UiIcon } from '@/ui'
+
+type Props = HTMLAttributes<HTMLDivElement> &
+  (
+    | {
+        route?: string
+        href?: never
+      }
+    | {
+        route?: never
+        href?: string
+      }
+  )
+
+const ROOT_CLASS_NAMES = cn(
+  'flex justify-center leading-[normal] not-italic gap-2 text-[color:var(--text-primary-main)] w-max text-5 font-normal underline',
+)
+
+export default function UiIconLink({ route, href, children, ...rest }: Props) {
+  const innerContent = (
+    <>
+      <span className='icon-link__text'>{children}</span>
+      <UiIcon
+        className={cn('size-5 text-inherit')}
+        name={IconNames.AcademicCap}
+      />
+    </>
+  )
+
+  if (route) {
+    return (
+      <NavLink className={cn(rest.className, ROOT_CLASS_NAMES)} to={route}>
+        {innerContent}
+      </NavLink>
+    )
+  }
+
+  return (
+    <a
+      className={cn(rest.className, ROOT_CLASS_NAMES)}
+      href={href}
+      target='_blank'
+      rel='noreferrer'
+    >
+      {innerContent}
+    </a>
+  )
+}
